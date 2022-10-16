e11cfff584da851fac391d0780648915eb3b3e25
Sashank Rampalli <sashankr97@gmail.com>
2022-10-16 16:37:03 +0530

    String Methods

--- C Sharp Basics/Program.cs
+++ C Sharp Basics/Program.cs
@@ -1,2 +1,8 @@
-﻿string name = "Sashank";
-Console.WriteLine($"Hello {name}");
+﻿string name = "Hello World";
+
+Console.WriteLine($"{name.Replace("World","Universe")}");
+Console.WriteLine($"{name.ToUpper()}");
+
+var result = name.Contains("World");
+
+Console.WriteLine($"Does {name} contain World?  {result}");
