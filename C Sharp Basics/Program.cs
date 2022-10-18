2af5f7417e1e8eab841c3353e6503ac0f32bded1
Sashank Rampalli <sashankr97@gmail.com>
2022-10-18 23:19:48 +0530

    Basic Data types

--- C Sharp Basics/Program.cs
+++ C Sharp Basics/Program.cs
@@ -6,3 +6,17 @@ This is the second line.
 
 Console.WriteLine("This is the first line.");
 Console.Write("This is the second line.");
+
+/*
+ Data Types in C#
+    1. Char
+    2. Int
+    3. Decimal
+    4. Bool
+ */
+
+Console.WriteLine('a');
+Console.WriteLine(12);
+Console.WriteLine(12.25m);
+Console.WriteLine(true);
+
